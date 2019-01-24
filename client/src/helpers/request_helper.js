9c18f29f23a742400122d267949e3ad57a8f3d13
Michael Irwin <mike.irwin1@gmail.com>
2019-01-24 16:28:18 +0000

    full setup complete. all dependencies installed, pubsub and request helper inputted

---
+++ client/src/helpers/request_helper.js
@@ -0,0 +1,26 @@
+const RequestHelper = function (url) {
+  this.url = url;
+}
+
+RequestHelper.prototype.get = function () {
+  return fetch(this.url)
+    .then((response) => response.json());
+};
+
+RequestHelper.prototype.post = function (item) {
+  return fetch(this.url, {
+    method: 'POST',
+    body: JSON.stringify(item),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((response) => response.json());
+};
+
+RequestHelper.prototype.delete = function (itemID) {
+  return fetch(`${this.url}/${itemID}`, {
+    method: 'DELETE'
+  })
+    .then((response) => response.json());
+};
+
+module.exports = RequestHelper;
