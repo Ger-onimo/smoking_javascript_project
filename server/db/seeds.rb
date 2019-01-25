89f570fa5504dfc6af835f9151fc991689b8f8b3
Joe McColl <joe4kool@gmail.com>
2019-01-25 11:08:53 +0000

    added dependencies server.js file

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,4 +1,4 @@
-use bucketList;
+use quitsmoking;
 db.dropDatabase();
 
 db.mustDos.insertMany([
--- server/server.js
+++ server/server.js
@@ -4,3 +4,6 @@ const app = express();
 const bodyParser = require('body-parser');
  const createRouter = require("./helpers/create_router.js");
  const MongoClient = require("mongodb").MongoCLient;
+
+ app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+ app.use(bodyParser.json());
