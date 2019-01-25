8ece455457047b1b6d40686996c70e8c5fe921c4
Joe McColl <joe4kool@gmail.com>
2019-01-25 11:45:11 +0000

    added expressRouter, error router undefined

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -2,7 +2,11 @@ const express  =  require('express');
 const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = function (collection) {
-  
+
+const router = express.Router()
+
+
+
 }
 
 
