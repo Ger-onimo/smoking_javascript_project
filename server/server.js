62a20d3e270efcb7eb959ecd23859e749631c8bc
Joe McColl <joe4kool@gmail.com>
2019-01-25 11:19:38 +0000

    server.js elements updated and only error is that createRouter doesn't exist, which is as expected ATM

--- server/server.js
+++ server/server.js
@@ -2,8 +2,22 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
- const createRouter = require("./helpers/create_router.js");
- const MongoClient = require("mongodb").MongoCLient;
+const createRouter = require("./helpers/create_router.js");
+const MongoClient = require("mongodb").MongoClient;
 
- app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
- app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+app.use(bodyParser.json());
+
+
+MongoClient.connect("mongodb://localhost:27017")
+.then((client) => {
+  const db = client.db("quitsmoking");
+  const cigaretteCollection = db.collection("cigarettes");
+  const cigaretteRouter = createRouter(cigaretteCollection);
+  app.use("/api/quitsmoking", cigaretteRouter)
+})
+.catch(console.error);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${this.address().port}`);
+})
