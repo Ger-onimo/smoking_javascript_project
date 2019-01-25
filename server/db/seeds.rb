764e96d389e72ed70b783425303ddcfd061f611f
Joe McColl <joe4kool@gmail.com>
2019-01-25 11:00:55 +0000

    created some initial seeds.js data

---
+++ server/db/seeds.rb
@@ -0,0 +1,23 @@
+use bucketList;
+db.dropDatabase();
+
+db.mustDos.insertMany([
+  {
+    brand: 'Lambert and Butler',
+    cost: 10,
+    daily: 20,
+    datetime: "2019-01-25 09:00:00"
+  },
+  {
+    brand: 'Marlboro',
+    cost: 11,
+    daily: 15,
+    datetime: "2019-01-25 09:00:00"
+  },
+  {
+    brand: 'Lucky Strike',
+    cost: 8,
+    daily: 30,
+    datetime: "2019-01-25 09:00:00"
+  },
+]);
