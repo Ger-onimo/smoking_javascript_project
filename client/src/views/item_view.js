21cbab3291202e9fd1084ba4378147f4bbfecd1c
Joe McColl <joe4kool@gmail.com>
2019-01-25 21:09:20 +0000

    Added initial li element for brand, displaying in browser within div / ul

--- client/src/views/item_view.js
+++ client/src/views/item_view.js
@@ -14,12 +14,23 @@ ItemView.prototype.render = function () {
   const detailList = this.createListElement();
   element.appendChild(detailList);
 
+  const cigaretteBrand = this.createBrandElement();
+  detailList.appendChild(cigaretteBrand);
+
+  this.element.appendChild(element);
+
 };
 
 ItemView.prototype.createListElement = function () {
   const list = document.createElement('ul');
   list.classList.add('smoker-detail-list');
   return list;
+};
+
+ItemView.prototype.createBrandElement = function () {
+  const brand = document.createElement('li');
+  brand.textContent = `Brand: ${this.itemData.brand}`;
+  return brand;
 };
 
 module.exports = ItemView;
