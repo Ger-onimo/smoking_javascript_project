ff093ea268fe6e42667d5996c1006cb1a3e1a14f
Michael Irwin <mike.irwin1@gmail.com>
2019-01-25 13:40:24 +0000

    refactored form view to make more expandable and reusable

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -12,33 +12,13 @@ FormView.prototype.bindEvents = function () {
 };
 
 FormView.prototype.createForm = function () {
-  const cigLabel = document.createElement('label');
-  cigLabel.for = 'cigBrand';
-  cigLabel.innerHTML = 'Brand:'
-  const cigType = document.createElement('input');
-  cigType.id = 'cigBrand';
-  cigType.type = 'text';
-  cigType.placeholder = 'Enter Brand..'
-  this.element.appendChild(cigLabel);
-  this.element.appendChild(cigType);
+  const labels = this.createLabels();
+  const inputs = this.createInputs();
 
-  const numLabel = document.createElement('label');
-  numLabel.for = 'cigNumber';
-  numLabel.innerHTML = 'Number per day:'
-  const cigNumber = document.createElement('input');
-  cigNumber.id = 'cigNumber';
-  cigNumber.type = 'number';
-  this.element.appendChild(numLabel);
-  this.element.appendChild(cigNumber);
-
-  const costLabel = document.createElement('label');
-  costLabel.for = 'cost';
-  costLabel.innerHTML = 'Price per pack';
-  const cost = document.createElement('input');
-  cost.id = 'cost';
-  cost.type = 'number';
-  this.element.appendChild(costLabel);
-  this.element.appendChild(cost);
+  for (let i = 0; i < labels.length; i++) {
+    this.element.appendChild(labels[i]);
+    this.element.appendChild(inputs[i])
+  }
 
   const submitFormButton = document.createElement('button');
   submitFormButton.type = 'submit';
@@ -54,6 +34,41 @@ FormView.prototype.handleFormSubmit = function (event) {
     cost: event.target.cost.value
   }
   console.log(newClientInfo);
+};
+
+FormView.prototype.createLabels = function () {
+  const cigLabel = document.createElement('label');
+  cigLabel.for = 'cigBrand';
+  cigLabel.innerHTML = 'Brand: ';
+
+  const numLabel = document.createElement('label');
+  numLabel.for = 'cigNumber';
+  numLabel.innerHTML = 'Number per day: '
+
+  const costLabel = document.createElement('label');
+  costLabel.for = 'cost';
+  costLabel.innerHTML = 'Price per pack (£): ';
+
+  const labels = [cigLabel, numLabel, costLabel];
+  return labels;
+};
+
+FormView.prototype.createInputs = function () {
+  const cigType = document.createElement('input');
+  cigType.id = 'cigBrand';
+  cigType.type = 'text';
+  cigType.placeholder = 'Enter Brand..'
+
+  const cigNumber = document.createElement('input');
+  cigNumber.id = 'cigNumber';
+  cigNumber.type = 'number';
+
+  const cost = document.createElement('input');
+  cost.id = 'cost';
+  cost.type = 'number';
+
+  const inputs = [cigType, cigNumber, cost];
+  return inputs;
 };
 
 module.exports = FormView;
