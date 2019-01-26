39076d6a654e1d2ae8efc77c20cdba65b0990823
Joe McColl <joe4kool@gmail.com>
2019-01-26 20:59:43 +0000

    cleaned up code

--- client/src/views/timer_view.js
+++ client/src/views/timer_view.js
@@ -21,44 +21,4 @@ TimerView.prototype.createTimer = function () {
 
 };
 
-// let time = document.getElementsByTagName('time')[0],
-// reset = document.getElementById('reset'),
-// seconds = 0, minutes = 0, hours = 0, days = 0,
-// t;
-//
-// function addTimePerUnit() {
-//   seconds++;
-//   if (seconds >= 60) {
-//     seconds = 0;
-//     minutes++;
-//     if (minutes >= 60) {
-//       minutes = 0;
-//       hours++;
-//       if (hours >= 24) {
-//         hours = 0;
-//         days++;
-//       }
-//     }
-//   }
-//
-//   time.textContent = (days ? (days > 9 ? days : "0" + days) : "00") + ":"
-//   + (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":"
-//   + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":"
-//   + (seconds > 9 ? seconds : "0" + seconds);
-//
-//   timer();
-// }
-//
-// function timer() {
-//   t = setTimeout(addTimePerUnit, 1000);
-// }
-//
-// timer();
-//
-// /* Reset button */
-// reset.onclick = function() {
-//   time.textContent = "00:00:00:00";
-//   seconds = 0; minutes = 0; hours = 0; days = 0;
-// }
-
 module.exports = TimerView;
