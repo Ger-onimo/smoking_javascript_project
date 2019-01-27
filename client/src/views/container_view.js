17f251d381fdd73dd4403dac93fd68e4d2d4ece7
Joe McColl <joe4kool@gmail.com>
2019-01-27 16:40:49 +0000

    removed timer functions from container view as refactor upon merge and need to understand

--- client/src/views/container_view.js
+++ client/src/views/container_view.js
@@ -1,7 +1,7 @@
 const PubSub = require('../helpers/pub_sub.js');
 const ItemView = require('./item_view.js');
 const SmokedView = require('./smoked_view.js');
-const TimerView = require('./timer_view.js'); /////timer
+
 
 const ContainerView = function (element) {
   this.element = element;
@@ -9,27 +9,17 @@ const ContainerView = function (element) {
 
 ContainerView.prototype.bindEvents = function() {
   this.createSmokedButton();
-  this.createTimer();  //timer
+
   PubSub.subscribe('Cigarettes:data-ready', (evt) => {
     const items = evt.detail;
     this.renderContainer(items);
   });
 };
 
-
 ContainerView.prototype.createSmokedButton = function () {
   const smokeButton = new SmokedView(this.element);
   smokeButton.bindEvents();
 };
-
-////////timer
-
-ContainerView.prototype.createTimer = function () {
-  const timer = new TimerView(this.element);
-  timer.bindEvents();
-};
-
-//////
 
 ContainerView.prototype.renderContainer = function (items) {
   items.forEach((item) => {
