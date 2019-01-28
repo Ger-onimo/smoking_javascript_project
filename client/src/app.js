30f054037ff4dbf42c4ab91020cd8ebdb321e620
Gabriele Filigheddu <gabriele.filigheddu@hotmail.com>
2019-01-28 10:19:18 +0000

    smoking facts motivation

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,20 @@
 const FormView = require('./views/form_view.js');
 const ContainerView = require('./views/container_view.js');
 const Cigarettes = require('./models/cigarettes.js');
+const RandMotivation = require('./models/smoking_facts.js') //
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Javascript Loaded');
   const form = document.querySelector('form#initial-entry-form');
   const formView = new FormView(form);
+  const messageElement = document.querySelector('p.message'); //
   formView.bindEvents()
 
 const cigarettes = new Cigarettes();
 cigarettes.bindEvents();
 
+const randMotivation = new RandMotivation(); //
+const motivation = randMotivation.get();  //
+const message = `  ${motivation}.`; //
+messageElement.textContent = message; //
 })
