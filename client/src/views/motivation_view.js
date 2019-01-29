adb6a11fb89e53b3b567c64b7f53ca8c1c868075
Joe McColl <joe4kool@gmail.com>
2019-01-29 11:02:08 +0000

    put random motive behind collapsible button, working in browser

--- client/src/views/motivation_view.js
+++ client/src/views/motivation_view.js
@@ -10,7 +10,7 @@ const randMotivation = new RandMotivation();
   PubSub.subscribe('FormView:new-user', (event) => {
     this.text = randMotivation.get();
     this.createMotivContainer();
-
+this.motiveButton();
 
   });
 
@@ -31,6 +31,23 @@ MotivationView.prototype.createMotivContainer = function () {
   motive.textContent = `${this.text}`;
   container.appendChild(motive);
   this.element.appendChild(container);
+};
+
+MotivationView.prototype.motiveButton = function () {
+  const button = document.getElementById('random-motive');
+  button.addEventListener('click', (event) => {
+    this.hideForm();
+  })
+};
+
+MotivationView.prototype.hideForm = function () {
+  let form = document.getElementById('motive-container');
+  if (form.style.display === 'block') {
+    form.style.display = 'none';
+  }
+  else {
+    form.style.display = 'block';
+  }
 };
 
 
